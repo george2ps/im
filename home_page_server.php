320cee74770381ce34b3ef81469d732c56e588d6
george sofroniou <george.sofroniou15@gmail.com>
2018-06-26 10:56:54 +0100

    sort clients

--- home_page_server.php
+++ home_page_server.php
@@ -18,11 +18,18 @@
 	$firstname = $row['firstname'];
 	$lastname = $row['lastname'];
 	$profilePicture = $row['profile_picture'];
+	$clientFirstnameArray = array();
+	$query2 = mysqli_query($conn, "select firstname from client_info where insurer_email='$username'");
 
-	$query2 = mysqli_query($conn, "select html_element from client_info where insurer_email='$username'");
-
+	while($row3 = mysqli_fetch_array($query2, MYSQLI_ASSOC)){
+		$clientFirstnameArray[] = $row3['firstname'];
+	}
+	rsort($clientFirstnameArray);
+	$arrayL= count($clientFirstnameArray);
 	$clientArray =array();
-	while($row2 = mysqli_fetch_array($query2, MYSQLI_ASSOC)){
+	for($i=0; $i<$arrayL; $i++){
+		$query3 = mysqli_query($conn, "select html_element from client_info where insurer_email='$username' AND firstname='$clientFirstnameArray[$i]'");
+		$row2 = mysqli_fetch_array($query3, MYSQLI_ASSOC);
 		$clientArray[] = $row2['html_element'];
 	}
 	$arrayLength = count($clientArray);
--- official/im.php
+++ official/im.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+body,h1,h2,h3,h4,h5,h6 {font-family: Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif';}
 body, html {
     height: 100%;
     line-height: 1.8;
@@ -27,6 +27,8 @@ body, html {
 		margin-left: 25%;
 	}
 </style>
+	
+
 <body>
 
 <!-- Navbar (sit on top) -->
@@ -68,9 +70,6 @@ body, html {
   <div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
     <i class="fa fa-facebook-official w3-hover-opacity"></i>
     <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
     <i class="fa fa-linkedin w3-hover-opacity"></i>
   </div>
 </header>
@@ -107,17 +106,30 @@ body, html {
 <div class="w3-container w3-light-grey" style="padding:128px 16px">
   <div class="w3-row-padding">
     <div class="w3-col m6">
-      <h3>We know design.</h3>
-      <p>The application is SPA (single page application) which has a well thought out and frindly user interface</p>
+      <h3>Well Designed</h3>
+      <p>The application is SPA (single page application) which has a well thought out and friendly user interface</p>
     </div>
     <div class="w3-col m6">
       <img class="w3-image w3-round-large" src="../style/images/sample.png" alt="Buildings" width="700" height="394">
     </div>
   </div>
 </div>
-
+	
+<!-- Promo Section - "We know design" -->
+<div class="w3-container w3-light" style="padding:128px 16px">
+  <div class="w3-row-padding">
+	  <div class="w3-col m6">
+      <img class="w3-image w3-round-large" src="../style/images/sample2.png" alt="Buildings" width="700" height="394">
+    </div>
+    <div class="w3-col m6">
+      <h3>Siimple Navaigation</h3>
+      <p>Clear buttons and color indications are responsible for the most efficient navigation inside the application</p>
+    </div>
+    
+  </div>
+</div>
 <!-- Team Section -->
-<center><div class="w3-container" style="padding:128px 16px" id="team">
+<center><div class="w3-container w3-light-gray" style="padding:128px 16px" id="team">
   <h3 class="w3-center">THE TEAM</h3>
   <p class="w3-center w3-large">The ones who run this company</p>
   <div class="w3-row-padding w3-grayscale" style="margin-top:64px">
@@ -128,7 +140,7 @@ body, html {
           <h3>Antonis Papayiannis</h3>
           <p class="w3-opacity">CEO & Founder</p>
           <p></p>
-          <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</button></p>
+          
         </div>
       </div>
     </div>
@@ -139,7 +151,7 @@ body, html {
           <h3>George Sofroniou</h3>
           <p class="w3-opacity">CEO & Founder</p>
           <p></p>
-          <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</button></p>
+          
         </div>
       </div>
     </div>
@@ -215,22 +227,6 @@ body, html {
       <p><i class="fa fa-map-marker fa-fw w3-xxlarge w3-margin-right"></i> Nicosia, Cyprus</p>
       <p><i class="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Phone: +35799034121, +35799245404 </p>
       <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email: cypro@gmail.com</p>
-      <br>
-      <form action="/action_page.php" target="_blank">
-        <p><input class="w3-input w3-border" type="text" placeholder="Name" required name="Name"></p>
-        <p><input class="w3-input w3-border" type="text" placeholder="Email" required name="Email"></p>
-        <p><input class="w3-input w3-border" type="text" placeholder="Subject" required name="Subject"></p>
-        <p><input class="w3-input w3-border" type="text" placeholder="Message" required name="Message"></p>
-        <p>
-          <button class="w3-button w3-black" type="submit">
-            <i class="fa fa-paper-plane"></i> SEND MESSAGE
-          </button>
-        </p>
-      </form>
-    </div>
-    <div class="w3-half">
-      <!-- Add Google Maps -->
-      <div id="googleMap" class="w3-greyscale-max" style="width:100%;height:510px;"></div>
     </div>
   </div>
 </div>
@@ -241,12 +237,26 @@ body, html {
   <div class="w3-xlarge w3-section">
     <i class="fa fa-facebook-official w3-hover-opacity"></i>
     <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
     <i class="fa fa-linkedin w3-hover-opacity"></i>
   </div>
   <p>Powered by <a href="#" title="W3.CSS" target="_blank" class="w3-hover-text-green">Cypro</a></p>
 </footer>
+<script>
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+</script>
 </body>
 </html>
